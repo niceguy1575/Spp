600daba569e86f29d0603942dc14f6068ecd378d
소윤섭 <solee5155@gmail.com>
2017-06-04 16:29:36 +0900

    Correct TCP Client Error

--- TcpClient.java
+++ TcpClient.java
@@ -1,8 +1,8 @@
-package tcp;
+package Client;
 
 import java.io.*;
 import java.net.*;
-import tcp.FileEvent;
+import metaEvent.*;
 
 public class TcpClient {
 	Socket socket;
@@ -38,13 +38,17 @@ public class TcpClient {
 		}
 	}
 	
-	public void receive() {
+	public String receive() {
+		String mesg = null;
 		try {
 			//서버 소켓으로부터 받은 메시지를 화면에 출력
-			System.out.println("[서버] "+ in.readLine());		
+//			System.out.println("[서버] "+ in.readLine());
+			mesg = in.readLine();
+			return mesg;
 		} catch (IOException e) {
 			System.out.println(e.toString());			
 		}
+		return mesg;
 	}
 	
 	public void send(String msg) {
@@ -54,59 +58,70 @@ public class TcpClient {
 		System.out.println("[클라이언트] " + msg);		
 	}
 	
-	public void sendFile() {
-		
+	public void sendFile(String srcPath) {
 		FileEvent fileEvent = new FileEvent();
 		
-		String fileName = srcPath.substring(srcPath.lastIndexOf("/") + 1, srcPath.length());
+		String fileName = srcPath.substring(srcPath.lastIndexOf("\\") + 1, srcPath.length());
 		//String path = srcPath.substring(0, srcPath.lastIndexOf("/") + 1);
-		
 		fileEvent.setDestDir(destPath);
 		fileEvent.setFilename(fileName);
 		fileEvent.setSrcDir(srcPath);
 		
 		File file = new File(srcPath);
-		
-		if (file.isFile()) {
-			try {
-				DataInputStream diStream = new DataInputStream(new FileInputStream(file));
-				long len = (int) file.length();
-				byte[] fileBytes = new byte[(int) len];
-				
-				int read = 0;
-				int numRead = 0;
-				while (read < fileBytes.length && (numRead = diStream.read(fileBytes, read, fileBytes.length - read)) >= 0) {
-					read = read + numRead;
+
+			if (file.isFile()) {
+				try {
+//					DataInputStream diStream = new DataInputStream(new FileInputStream(file));
+					long len = (int) file.length();
+					byte[] fileBytes = new byte[(int) len];
+
+//					int read = 0;
+//					int numRead = 0;
+//					while (read < fileBytes.length && (numRead = diStream.read(fileBytes, read, fileBytes.length - read)) >= 0) {
+//						read = read + numRead;
+////					    if(){
+////		                     System.exit(0);
+////		                  }
+//					}
+//					
+					long startTime = System.currentTimeMillis();
+					fileEvent.settime(startTime);
+					fileEvent.setFileSize(len);
+					fileEvent.setFileData(fileBytes);
+					fileEvent.setStatus("Success");
+					fileEvent.setCRC32Value(crc.getCRC32(srcPath,fileBytes));
+					
+					System.out.println((int) fileBytes.length+"\n");
+
+					for(int i=0; i<fileBytes.length; i++){
+						System.out.println(fileBytes[i]);
+					}
+//					System.out.println();
+//					for(int i=0; i<sendBytes.length; i++){
+//					System.out.println(sendBytes[i]);
+//			}
+					
+//					diStream.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+					fileEvent.setStatus("Error");
 				}
-				long startTime = System.currentTimeMillis();
-				fileEvent.settime(startTime);
-				fileEvent.setFileSize(len);
-				fileEvent.setFileData(fileBytes);
-				fileEvent.setStatus("Success");
-				fileEvent.setCRC32Value(crc.getCRC32(srcPath,fileBytes));
-				
-				diStream.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+			} else {
+				System.out.println("path is not pointing to a file");
 				fileEvent.setStatus("Error");
+				System.out.println("TCP client를 종료합니다.");
+				System.exit(0);
 			}
-		} else {
-			System.out.println("path is not pointing to a file");
-			fileEvent.setStatus("Error");
-			System.out.println("TCP client를 종료합니다.");
-			System.exit(0);
-		}
-		
-		// write file
-		try {
-			outputStream.writeObject(fileEvent);
-			System.out.println("파일 전송 완료");
-			Thread.sleep(3000);
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+			// write file
+			try {
+				outputStream.writeObject(fileEvent);
+				System.out.println("파일 전송 완료");
+				Thread.sleep(3000);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 	}
 	
 	public void close() {
@@ -124,14 +139,5 @@ public class TcpClient {
 		System.out.println("     서버 주소: " + socket.getInetAddress());
 		System.out.println("     서버 포트번호: " + socket.getPort());
 		System.out.println("     클라이인트 포트번호: " + socket.getLocalPort() + '\n');
-	}
-	
-	public static void main(String[] args) {
-		//서버 주소와 포트번호를 지정하여 서버에 접속
-		TcpClient client = new TcpClient("192.168.0.18", 8000, "C:/Users/YunSeob/Downloads/ch03.ppt", "C:/Users/YunSeob/Downloads/test/");
-		client.sendFile();
-		client.receive();
-		client.close();
-		
 	}
 }
